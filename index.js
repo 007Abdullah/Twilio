e4e62c6235c9de0b733176d2d2d7ff0889779525
Muhammad Abdullah <kb337137@gmail.com>
2021-09-07 15:18:59 +0500

    user otp and code send phone number

---
+++ index.js
@@ -0,0 +1,86 @@
+const PORT = process.env.PORT || 5000;
+const express = require('express');
+var cookieParser = require('cookie-parser');
+var cors = require("cors");
+var morgan = require("morgan");
+var http = require("http");
+const client = require('twilio')('AC20cc2c5810301f9759dcccc5cc42163b', '8fd618f8f3f3b2aeb09a8918616471ce');
+const app = express();
+var server = http.createServer(app);
+var { userNumber } = require('./mongose');
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(cors({
+    origin: '*',
+    credentials: true
+}));
+
+app.use(morgan('dev'));
+app.get('/', (req, res, next) => {
+    res.send(`
+        <div style="text-align:center;padding-top:40px;">
+            <h1>Welcome to My Website</h1>
+            <h1>Your are message send Successfully</h1>
+            <p>This is a Hello World App</p>
+        </div>
+    `)
+})
+
+app.post('/UserNumber', (req, res, next) => {
+    if (!req.body.phoneNumber) {
+        res.send({
+            message: "phoneNumber Required",
+            status: 404
+        });
+        return;
+    }
+    const otp = Math.floor(getRandomArbitrary(111111, 999999));
+    userNumber.create({
+        'PhoneNumber': req.body.phoneNumber,
+        'otpCode': otp,
+    }).then((doc) => {
+        console.log('Mogonse Doc :', doc);
+        client.messages.create({
+            body: `OTP CODE :${otp}`,
+            to: '+923121278181',
+            from: '+14804181704',
+        }).then((message) => {
+            console.log('Message :', message);
+            res.send({
+                message: `Message Has Been Send :${message} `,
+                status: '200'
+            });
+        }).catch((error) => {
+            console.log('Error :', error);
+            res.send({
+                message: `Unexpected Error ${error}`,
+                status: 500,
+            });
+        });
+    });
+})
+app.post('/phoneUserPassword', (req, res, next) => {
+    if (!req.body.Password) {
+        res.send({
+            message: 'Please Required Password',
+            status:'404',
+        });
+    }
+    
+})
+
+
+
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server Started On Port`, PORT);
+});
---
+++ mongose.js
@@ -0,0 +1,40 @@
+var mongoose = require('mongoose');
+
+let dbURI = "mongodb+srv://root:root@cluster0.s5oku.mongodb.net/twilio?retryWrites=true&w=majority";
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+mongoose.connection.on("connected", function () {
+    console.log("Mongoose Is Connected");
+});
+mongoose.connection.on("disconnected", function () {
+    console.log("Mongoose Is Disconnected");
+    process.exit(1);
+});
+mongoose.connection.on("error", function (err) {
+    console.log("Mongoose connection error ", err);
+    process.exit(1);
+});
+
+process.on('SIGINT', function () {
+    console.log("App is terminated");
+    mongoose.connection.close(function () {
+        console.log("Mongoose default connection closed");
+        process.exit(0);
+    });
+});
+
+var userNumberSchema = mongoose.Schema({
+    'PhoneNumber': String,
+    'otpCode': String,
+    'createdOn': { 'type': Date, 'default': Date.now },
+    'activeSince': Date,
+});
+
+var userNumber = mongoose.model('phoneNumber', userNumberSchema);
+
+
+module.exports = ({
+    userNumber: userNumber,
+});
